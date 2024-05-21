63168a63a0babefbe08b914a328ca43bfdaf07db
Cedric Durau <cedric.durau@remondis.de>
2024-05-21 14:28:23 +0200

    Enable Vite PWA in DEV

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),
-  VitePWA({ registerType: 'autoUpdate' })
+  VitePWA({
+    registerType: 'autoUpdate',
+    devOptions: {
+      enabled: true
+    }
+  })
   ],
 })
